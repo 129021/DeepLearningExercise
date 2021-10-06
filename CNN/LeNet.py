dad890873124fa7edb09de3ba609121b26463e2b
129021 <wangjianqin1@yahoo.com>
2021-10-06 11:14:36 +0800

    LeNet write done

--- CNN/LeNet.py
+++ CNN/LeNet.py
@@ -29,3 +29,62 @@ train_iter,test_iter=d2l.load_data_fashion_mnist(batch_size=batch_size)
 
 '''对evaluate_accuracy函数进行轻微的修改'''
 def evaluate_accuracy_gpu(net,data_iter,device=None):
+    '''使用gpu计算模型在数据集上的精度'''
+    if isinstance(net,torch.nn.Module):
+        net.eval()
+        if not device:
+            device=next(iter(net.parameters())).device
+    metric=d2l.Accumulator(2)
+    for X,y in data_iter:
+        if isinstance(X,list):
+            X=[x.to(device)for x in X]
+        else:
+            X=X.to(device)
+        y=y.to(device)
+        metric.add(d2l.accuracy(net(X),y),y.numel())
+    return metric[0]/metric[1]
+
+
+def train_ch6(net,train_iter,test_iter,num_epochs,lr,device):
+    '''Trian a model with a GPU '''
+    def init_weights(m):
+        if type(m)==nn.Linear or type(m)==nn.Conv2d:
+            nn.init.xavier_uniform_(m.weight)
+    net.apply(init_weights)
+    print('training on',device)
+    net.to(device)
+    optimizer=torch.optim.SGD(net.parameters(),lr=lr)
+    loss=nn.CrossEntropyLoss()
+    '''动画效果'''
+    animator=d2l.Animator(xlabel='epoch',xlim=[1,num_epochs],
+                          legend=['train loss','train acc','test acc'])
+    timer,num_batches=d2l.Timer(),len(train_iter)
+    for epoch in range(num_epochs):
+        metric=d2l.Accumulator(3)
+        net.train()
+        for i,(X,y) in enumerate(train_iter):
+            timer.start()
+            optimizer.zero_grad()
+            X,y=X.to(device),y.to(device)
+            y_hat=net(X)
+            l=loss(y_hat,y)
+            l.backward()
+            optimizer.step()
+            metric.add(1*X.shape[0],d2l.accuracy(y_hat,y),X.shape[0])
+            timer.stop()
+            train_1=metric[0]/metric[2]
+            train_acc=metric[1]/metric[2]
+            if (i+1)%(num_batches//5)==0 or i==num_batches-1:
+                animator.add(epoch+(i+1)/num_batches,
+                             (train_1,train_acc,None))
+        test_acc=evaluate_accuracy_gpu(net,test_iter)
+        animator.add(epoch+1,(None,None,test_acc))
+    print(f'loss {train_1:.3f},tain acc {train_acc:.3f},'
+          f'test acc {test_acc:.3f}')
+    print(f'{metric[2]*num_epochs/timer.sum():.1f} examples/sec'
+          f'on {str(device)}')
+
+
+'''训练和评估LeNet-5模型'''
+lr,num_epochs=0.9,10
+train_ch6(net,train_iter,test_iter,num_epochs,lr,d2l.try_gpu())
