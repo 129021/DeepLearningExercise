906c52b9577226d62975408bf8ef5b1937fea1b9
129021 <wangjianqin1@yahoo.com>
2021-10-13 15:57:10 +0800

    fine_tuning finished

--- ComputerVision/fine-tuning.py
+++ ComputerVision/fine-tuning.py
@@ -43,5 +43,40 @@ finetune_net=torchvision.models.resnet18(pretrained=True)
 finetune_net.fc=nn.Linear(finetune_net.fc.in_features,2)
 nn.init.xavier_uniform_(finetune_net.fc.weight)
 
-#微调模型
+'''微调模型'''
+#如果param_group=True,输出层中的模型参数将使用10倍的学习率
+def train_fine_tuning(net,learning_rate,batch_size=128,num_epochs=5,param_group=True):
+    train_iter=torch.utils.data.DataLoader(
+        torchvision.datasets.ImageFolder(os.path.join(data_dir,'train'),transform=train_augs),
+        batch_size=batch_size,shuffle=True
+    )
+    test_iter=torch.utils.data.DataLoader(
+        torchvision.datasets.ImageFolder(os.path.join(data_dir,'test'),transform=test_augs),
+        batch_size=batch_size
+    )
+    devices=d2l.try_all_gpus()
+    loss=nn.CrossEntropyLoss(reduction='none')
+    if param_group:
+        params_1x=[
+            param for name,param in net.named_parameters()
+            if name not in ['fc.weight','fc.bias']
+        ]
+        trainer=torch.optim.SGD([
+            {'params':params_1x},
+            {'params':net.fc.parameters(),
+             'lr':learning_rate*10}
+        ],lr=learning_rate,
+        weight_decay=0.001)
+    else:
+        trainer=torch.optim.SGD(net.parameters(),lr=learning_rate,
+                                weight_decay=0.001)
+    d2l.train_ch13(net,train_iter,test_iter,loss,trainer,num_epochs,devices)
 
+#使用较小的学习率
+train_fine_tuning(finetune_net,5e-5)
+
+
+#为进行对比，所有模型参数初始化为随机值
+scratch_net=torchvision.models.resnet18()
+scratch_net.fc=nn.Linear(scratch_net.fc.in_features,2)
+train_fine_tuning(scratch_net,5e-4,param_group=False)
